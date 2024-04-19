0ec2ed0211fe8b94d7779bae6ddd0f084facd6cc
Gernot Fuchs <fuchsgernot@protonmail.com>
2024-04-19 18:00:20 +0200

    Save last cursor position in file
    Issue: #master

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,3 +1,10 @@
+-- Save last cursor postion in file
+vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+  desc = 'return cursor to where it was last time closing the file',
+  pattern = '*',
+  command = 'silent! normal! g`"zv',
+})
+
 -- Clear last command in command line after timeout
 vim.api.nvim_create_autocmd('CmdlineLeave', {
   callback = function()
